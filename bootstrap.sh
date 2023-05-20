6025bdeaa5a33f31652269b2f4ce5bf7fae45ab6
Jesse Zhu <jesse@itjesse.com>
2023-05-20 23:37:55 +0900

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 if [ -z "$OPTIMIZER_CONFIG_JS" ]; then
-  echo "Environment variable OPTIMIZER_CONFIG_JS is not set."
-  exit 1
+  echo "Environment variable OPTIMIZER_CONFIG_JS is not set. Skipping file generation."
+else
+  # 生成 optimizer.config.js 文件
+  echo "Generating optimizer.config.js"
+  echo "$OPTIMIZER_CONFIG_JS" > optimizer.config.js
 fi
-
-# 生成 optimizer.config.js 文件
-echo "Generating optimizer.config.js"
-echo "$OPTIMIZER_CONFIG_JS" > optimizer.config.js
 
 # 运行 yarn start
 echo "Running yarn start"

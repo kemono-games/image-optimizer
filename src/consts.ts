18efebf4875dec8cc285660bd0339d3174b4d4fb
Jesse Zhu <jesse@itjesse.com>
2023-08-30 00:42:53 +0900

    support apng

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,8 @@ export const PNG = 'image/png'
 export const JPEG = 'image/jpeg'
 export const GIF = 'image/gif'
 export const SVG = 'image/svg+xml'
+export const APNG = 'image/apng'
 
-export const supportedFormats = [AVIF, WEBP, PNG, JPEG, GIF, SVG]
+export const supportedFormats = [AVIF, WEBP, PNG, JPEG, GIF, SVG, APNG]
 export const supportedTargetFormats = [WEBP, PNG, JPEG]
 export const returnOriginalFormats = [SVG]
